5001a78b1cfd9b3012ef9bf2247e542296e5d785
hiogawa <hi.ogawa.zz@gmail.com>
2016-06-16 20:55:03 +0900

    move "terminal things" from kernel.c to terminal.c

--- workdir/include/system.h
+++ workdir/include/system.h
@@ -4,6 +4,12 @@
 #include <stddef.h> // size_t
 #include <stdint.h> // uint?_t
 
+// string.c
 size_t strlen(const char* str);
+
+// terminal.c
+void terminal_initialize();
+void terminal_writestring(const char* data);
+
 
 #endif
