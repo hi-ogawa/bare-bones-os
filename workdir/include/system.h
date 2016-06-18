dbdecf0f4a1f3cc6e11f9600ec506781e3f1c362
hiogawa <hi.ogawa.zz@gmail.com>
2016-06-19 00:01:08 +0900

    add io.c (implement c wrapper for inb/outb)

--- workdir/include/system.h
+++ workdir/include/system.h
@@ -30,6 +30,10 @@ void idt_set_gate(uint8_t num, uint32_t base, uint16_t selector, uint8_t flags);
 // isrs.c
 void isrs_install();
 
+// io.c
+uint8_t inb(uint16_t port);
+void outb(uint16_t port, uint8_t val);
+
 // dump.c
 void hexdump(uint32_t *addr, uint32_t size);
 
